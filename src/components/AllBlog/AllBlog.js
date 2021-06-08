a7a1abd2fe151002f065b2f0fb2dc75af076f484
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-06-08 10:34:25 +0600

    portfolio updated

--- src/components/AllBlog/AllBlog.js
+++ src/components/AllBlog/AllBlog.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import allBlogs from '../../Data/blogs';
-import AllBlogInfo from '../AllBlogInfo/AllBlogInfo';
-import Footer from '../Shared/Footer/Footer';
-import Bounce from 'react-reveal/Bounce';
+import React from "react";
+import allBlogs from "../../Data/blogs";
+import AllBlogInfo from "../AllBlogInfo/AllBlogInfo";
+import Footer from "../Shared/Footer/Footer";
+import Fade from "react-reveal/Fade";
 
 const AllBlog = () => {
-    return (
-        <div className="text-white">
-            <div className="container text-center">
-                <div className="pt-5">
-                    <Bounce left>
-                        <h3 className="bottom-border">MY BLOG</h3>
-                    </Bounce>
-                </div>
-                <Bounce right>
-                <div className="row pt-5">
-                    
-                        {
-                            allBlogs.map((blog, index) => <AllBlogInfo key={index} blog={blog}></AllBlogInfo>)
-                        }
-                    
-                </div>
-                </Bounce>
-                <h3 className="pt-5">Blog is coming soon!!!</h3>
-            </div>
-            <div>
-                <Footer></Footer>
-            </div>
+  return (
+    <div className="text-white">
+      <Fade>
+        <div className="container text-center">
+          <div className="pt-5">
+            <h3 className="bottom-border">MY BLOG</h3>
+          </div>
+          <div className="row pt-5">
+            {allBlogs.map((blog, index) => (
+              <AllBlogInfo key={index} blog={blog}></AllBlogInfo>
+            ))}
+          </div>
+          <h3 className="pt-5">Blog is coming soon!!!</h3>
         </div>
-    );
+      </Fade>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default AllBlog;
--- src/components/Home/Blog/Blog.js
+++ src/components/Home/Blog/Blog.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import BlogInfo from '../BlogInfo/BlogInfo';
-import blogs from '../../../Data/blogs';
-import { useHistory } from 'react-router';
-import Zoom from 'react-reveal/Zoom';
+import React from "react";
+import BlogInfo from "../BlogInfo/BlogInfo";
+import blogs from "../../../Data/blogs";
+import {useHistory} from "react-router";
+import Fade from "react-reveal/Fade";
 
 const Blog = () => {
-    const history = useHistory();
-    const handleClick = () => {
-        history.push('/blog');
-    }
-    return (
-        <div className="text-center">
-            <Zoom bottom>
-                <div className="margin text-white">
-                    <h3 className="bottom-border">MY BLOG</h3>
-                </div>
-                <div className="row">
-                    {
-                        blogs.map((blog, index) => <BlogInfo key={index} blog={blog}></BlogInfo>)
-                    }
-                </div>
-                <button onClick={handleClick} className="btn btn-outline-success mt-5">SEE MORE BLOGS</button>
-            </Zoom>
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/blog");
+  };
+  return (
+    <div className="text-center">
+      <Fade bottom>
+        <div className="margin text-white">
+          <h3 className="bottom-border">MY BLOG</h3>
         </div>
-    );
+        <div className="row">
+          {blogs.map((blog, index) => (
+            <BlogInfo key={index} blog={blog}></BlogInfo>
+          ))}
+        </div>
+        <button onClick={handleClick} className="btn btn-outline-success mt-5">
+          SEE MORE BLOGS
+        </button>
+      </Fade>
+    </div>
+  );
 };
 
 export default Blog;
